ec6a4befc7e4c73d305b85792c27f4f68d7dd7a1
shnmj <shnmje@gmail.com>
2024-04-25 14:15:05 +0900

    2. Axios (Data 뽑아내서 html으로 출력 - 버스정류장)

--- src/main/java/com/green/controller/BusanBIMSController.java
+++ src/main/java/com/green/controller/BusanBIMSController.java
@@ -11,43 +11,60 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import com.green.dto.BusInfoDto;
+
+@Controller      // .jsp, .mustache 를 리턴 / .html 리턴하려면 @RestController 하거나 @ResponseBody
 public class BusanBIMSController {
-	@RequestMapping("GetBus")
-	@ResponseBody // or restcont를 만들던지
-	public String getbus(
-			
-			) throws IOException {
-		
-		String serviceKey = "ZuqdMK%2B0Sw5M6w4OklL3zqfIaQrsvQP2PJdeT72p%2FDIv%2FUsSNiXz%2FpTDpn5pnPRLHfgW0zehKJh2fXd5ICSHZw%3D%3D";
-		
-		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6260000/BusanBIMS/busStopList"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode("부산시청", "UTF-8")); /*정류소 명*/
-        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode("13708", "UTF-8")); /*정류소 번호*/
+
+   @RequestMapping("/GetBus")
+   @ResponseBody
+   public String getbus(
+		   BusInfoDto dto   // name for argu 해결방법 
+		   /*
+			@RequestParam("numOfRows") String numOfRows, 
+			// IllegalArgumentException: Name for argument of type
+			@RequestParam("pageNo") String pageNo,
+			@RequestParam("bstopnm") String bstopnm, 
+			@RequestParam("arsno") String arsno
+			*/
+		   ) throws IOException { 
+      
+      String serviceKey = "ZuqdMK%2B0Sw5M6w4OklL3zqfIaQrsvQP2PJdeT72p%2FDIv%2FUsSNiXz%2FpTDpn5pnPRLHfgW0zehKJh2fXd5ICSHZw%3D%3D";
+      
+      StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6260000/BusanBIMS/busStopList"); // URL
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey);                           // Service Key
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(dto.getPageNo(), "UTF-8"));        // 페이지번호
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(dto.getNumOfRows(), "UTF-8")); // 한 페이지 결과 수
+        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode(dto.getBstopnm(), "UTF-8"));      // 정류소 명
+        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode(dto.getArsno(), "UTF-8"));          // 정류소 번호
+        
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
+        
         BufferedReader rd;
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         } else {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
+        
         StringBuilder sb = new StringBuilder();
         String line;
         while ((line = rd.readLine()) != null) {
             sb.append(line);
         }
+        
         rd.close();
         conn.disconnect();
         System.out.println(sb.toString());
-        
+      
         return sb.toString();
-		
-	}
+   
+   
+      
+   }
+   
 }
---
+++ src/main/java/com/green/dto/BusInfoDto.java
@@ -0,0 +1,12 @@
+package com.green.dto;
+
+import lombok.Data;
+
+@Data
+public class BusInfoDto {
+	private String numOfRows;
+	private String pageNo;
+	private String bstopnm;
+	private String arsno;
+	
+}
